2f6f15d54e042c3356eb937244b49b1ff1033267
Akash-node <aakashnegi645@gmail.com>
2025-09-01 22:29:25 +0530

    minor changes

--- src/routes/booking.route.js
+++ src/routes/booking.route.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.route("/eventBooking/:id").post(verifyJwt,createBooking);
 router.route("/verifyPayment/:id").post(verifyJwt,verifyPayment);
 router.route("/alreadyRegister/:id").get(verifyJwt,userAlreadyRegisterOrNot);
-router.route("/upadteUserAttendence").patch(verifyJwt,isAdmin,upadteUserAttendence);
+router.route("/updateUserAttendence").patch(verifyJwt,isAdmin,upadteUserAttendence);
 router.route("/totalAttendUser/:eventId").get(verifyJwt,attendedUsers);
 
 module.exports = router;
